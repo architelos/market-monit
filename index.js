0ab4fa8d19e7acc08ddae64567b7b1703d25f4f1
architelos <technopiabcde@gmail.com>
2022-08-13 14:45:46 +0700

    feat: add error handling

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const asciiArt = require('figlet')
 console.log(`${asciiArt.textSync('market-monit')}`)
 console.log(`Pixel Starships market monitoring tool by architelos#6702 - ${consoleColors.bold('Use at your own discretion.')}\n`)
 
+process.on('unhandledRejection', (reason, promise) => {
+    console.log(`${consoleColors.red('Unhandled promise rejection:')} ${reason}`)
+    process.exit(1)
+})
+
+process.on('uncaughtException', (error, location) => {
+    console.log(`${consoleColors.red('Uncaught exception:')} ${error}`)
+    process.exit(1)
+})
+
 getAccessToken().then(accessToken => {
     if (!accessToken.match(/.{8}-.{4}-/)) {
         console.log(`${consoleColors.red('Login failed:')} ${accessToken}`)
