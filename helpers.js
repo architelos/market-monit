240894951792c04bb73d0fb401705f891ccbadae
architelos <technopiabcde@gmail.com>
2022-08-13 22:10:25 +0700

    Some code refactoring, added current listings

--- helpers.js
+++ helpers.js
@@ -2,8 +2,10 @@ const client = require('axios').default
 const hasher = require('md5')
 const parser = require('xml-js')
 const crypto = require('crypto')
-const deviceTypes = ['DeviceTypeWindows', 'DeviceTypeAndroid', 'DeviceTypeMac', 'DeviceTypeIPhone', 'DeviceTypeGeneric']
+const consoleColors = require('chalk')
+const asciiArt = require('figlet')
 
+const deviceTypes = ['DeviceTypeWindows', 'DeviceTypeAndroid', 'DeviceTypeMac', 'DeviceTypeIPhone', 'DeviceTypeGeneric']
 const loginData = {
     deviceKey: crypto.randomBytes(8).toString('hex'),
     dateTime: new Date().toISOString().slice(0, -5),
@@ -17,13 +19,41 @@ async function getAccessToken() {
     try {
         const accessTokenXml = await client.post(`${baseUrl}/UserService/DeviceLogin11?deviceType=${loginData.deviceType}&deviceKey=${loginData.deviceKey}&checksum=${deviceChecksum}&clientDateTime=${loginData.dateTime}${loginData.uselessParams}`)
         const accessTokenJs = parser.xml2js(accessTokenXml.data, {compact: true, spaces: 4})
-        const isError = accessTokenJs.UserLogin
-        const output = (isError) ? accessTokenJs.UserLogin._attributes.errorMessage : accessTokenJs.UserService.UserLogin._attributes.accessToken
+        const isTokenError = accessTokenJs.UserLogin
+        const accessToken = isTokenError
+                            ? accessTokenJs.UserLogin._attributes.errorMessage
+                            : accessTokenJs.UserService.UserLogin._attributes.accessToken
 
-        return output
+        return accessToken
     } catch (error) {return error}
 }
 
+async function getCurrentListings(accessToken, itemId) {
+    try {
+        const currentListingsXml = await client.get(`${baseUrl}/MessageService/ListActiveMarketplaceMessages5?&itemDesignId=${itemId}&accessToken=${accessToken}&itemSubType=None&rarity=None&currencyType=Unknown&userId=0`)
+        const currentListingsJs = parser.xml2js(currentListingsXml.data, {compact: true, spaces: 4})
+        const isListingError = currentListingsJs.ListActiveMarketplaceMessages
+        const currentListings = isListingError
+                                ? currentListingsJs.ListActiveMarketplaceMessages._attributes.errorMessage
+                                : currentListingsJs.MessageService.ListActiveMarketplaceMessages.Messages.Message
+
+        return currentListings
+    } catch (error) {return error}
+}
+
+function welcomeMessage() {
+    console.log(`${asciiArt.textSync('market-monit')}`)
+    console.log(`Pixel Starships market monitoring tool by architelos#6702 - ${consoleColors.bold('Use at your own discretion.')}\n`)
+}
+
+function clearLastLine() {
+    process.stdout.moveCursor(0, -1)
+    process.stdout.clearLine(1)
+}
+
 module.exports = {
-    getAccessToken
+    getAccessToken,
+    getCurrentListings,
+    welcomeMessage,
+    clearLastLine,
 }
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const {getAccessToken} = require('./helpers')
+const {getAccessToken, getCurrentListings, welcomeMessage, clearLastLine} = require('./helpers')
 const consoleColors = require('chalk')
-const asciiArt = require('figlet')
-
-console.log(`${asciiArt.textSync('market-monit')}`)
-console.log(`Pixel Starships market monitoring tool by architelos#6702 - ${consoleColors.bold('Use at your own discretion.')}\n`)
+const currentAccessToken = new Array()
 
 process.on('unhandledRejection', (reason, promise) => {
     console.log(`${consoleColors.red('Unhandled promise rejection:')} ${reason}`)
@@ -15,11 +12,35 @@ process.on('uncaughtException', (error, location) => {
     process.exit(1)
 })
 
-getAccessToken().then(accessToken => {
+welcomeMessage()
+
+async function setAccessToken() {
+    console.log('Logging in...')
+
+    const accessToken = 'c27f2dbb-f2c3-4090-9a18-c24ef3d05ce0'
     if (!accessToken.match(/.{8}-.{4}-/)) {
+        clearLastLine()
         console.log(`${consoleColors.red('Login failed:')} ${accessToken}`)
         process.exit(1)
     }
 
-    console.log(`${consoleColors.green('Login succeeded!')}`)
-})
+    clearLastLine()
+    console.log(`${consoleColors.green('Login succeeded:')} ${accessToken}`)
+    currentAccessToken.push(accessToken)
+}
+
+setInterval(setAccessToken, 288 * 100000)
+setAccessToken()
+
+const accessToken = currentAccessToken.pop()
+
+async function marketMonit() {
+    console.log('Scanning market...')
+
+    const currentListings = await getCurrentListings(accessToken, 81)
+    clearLastLine()
+
+    console.log(currentListings.at(-1))
+}
+
+if (require.main == module) setInterval(marketMonit, 3000)
