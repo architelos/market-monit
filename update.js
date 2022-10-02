063032a2a91679a0c30afc62c6d888ceeade3274
architelos <108888162+architelos@users.noreply.github.com>
2022-10-02 21:49:50 +0700

    Fixing i being a global variable

--- update.js
+++ update.js
@@ -7,7 +7,7 @@ async function main() {
     const itemData = await axios.get(`https://api.pixelstarships.com/ItemService/ListItemDesigns2?languageKey=en`)
     const itemArray = parser.xml2js(itemData.data, {compact: true, spaces: 4}).ItemService.ListItemDesigns.ItemDesigns.ItemDesign
 
-    for (i in itemArray) {
+    for (const i in itemArray) {
         const currentData = itemArray[i]
         if (currentData._attributes.FairPrice != 0) {
             const itemDesignId = currentData._attributes.ItemDesignId
